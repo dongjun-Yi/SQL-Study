cf914341486d167dfa76297d5e0cf2e9b3ce3001
dongjun-Yi <djyi0223@gmail.com>
2023-10-16 20:57:26 +0900

    Basic Select/Weather Observation Station 5

---
+++ Hackerrank/Basic Select/Weather Observation Station 5/Weather Observation Station 5.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+*/
+
+select CITY, length(CITY)
+from STATION
+order by length(CITY), CITY
+limit 1;
+
+select CITY, length(CITY)
+from STATION
+order by length(CITY) desc, CITY
+limit 1;
